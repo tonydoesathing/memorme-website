9260b06941e0d85620303740e742d71dbcd8d4a5
MemorMe Admin <memormeapp@gmail.com>
2021-09-14 11:12:23 +0200

    Cleaned up commented code

--- components/Clouds.vue
+++ components/Clouds.vue
@@ -3,16 +3,6 @@
 </template>
 
 <style>
-/*.Clouds {
-  animation: 1s appear;
-  display: block;
-  margin: 0 auto;
-  height: 30%;
-  overflow: hidden;
-  overflow-x: hidden;
-  right: 50%;
-  transform: translateX(-45%);
-}*/
 
 @keyframes appear {
   0% {
